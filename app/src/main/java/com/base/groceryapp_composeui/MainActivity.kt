d9ee580af10e3f9c42f24552620a87f4babf18e8
fenil09 <backupsystem9909@gmail.com>
2023-07-14 11:21:51 +0530

    Initial commit

---
+++ app/src/main/java/com/base/groceryapp_composeui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.base.groceryapp_composeui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.base.groceryapp_composeui.ui.theme.GroceryApp_ComposeUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
